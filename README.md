be8ed6396be6ec34a9c3aeb02f255916fa567e62
kellworks <kchidothi@outlook.com>
2023-09-06 11:06:19 +0200

    refactor cli-scripts, readme.md

--- README.md
+++ README.md
@@ -1,20 +1,16 @@
 
-# Svg-Vue
-
+# Vue Svgify
 Converts svg files into vue typescript components.
 
-
 [![MIT License](https://img.shields.io/badge/logo-javascript-blue?logo=javascript)](https://choosealicense.com/licenses/mit/)
-
-
 
 ## Installation
 
 
-```npm install```
-    
+```npm install -g vue-svgify```
+
 ## Run
 
 
 ```node index.js --svg-folder-path --vue-component-output-path```
-    
+
--- index.js
+++ index.js
@@ -8,40 +8,45 @@ const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-const svgFolderPath = process.argv[2];
-const outputComponentPath = process.argv[3];
+function convert() {
+    const svgFolderPath = process.argv[2];
+    const outputComponentPath = process.argv[3];
 
-if (!svgFolderPath || !outputComponentPath) {
-  console.error('Usage: node index.js <SVG_FOLDER_PATH> <OUTPUT_COMPONENT_PATH>');
-  process.exit(1);
+    if (!svgFolderPath || !outputComponentPath) {
+        console.error('Usage: node index.js <SVG_FOLDER_PATH> <OUTPUT_COMPONENT_PATH>');
+        process.exit(1);
+    }
+
+    fs.readdir(svgFolderPath, (err, files) => {
+        if (err) {
+            console.error(`Error reading SVG folder: ${err}`);
+            process.exit(1);
+        }
+
+        files.forEach((file) => {
+            if (path.extname(file).toLowerCase() === '.svg') {
+                const svgFilePath = path.join(svgFolderPath, file);
+                fs.readFile(svgFilePath, 'utf8', (err, data) => {
+                    if (err) {
+                        console.error(`Error reading SVG file: ${err}`);
+                        return;
+                    }
+                    const $ = cheerio.load(data);
+                    const svgContent = $('svg').html();
+                    if (!svgContent) {
+                        console.error(`SVG content not found in ${svgFilePath}. Skipping.`);
+                        return;
+                    }
+                    const svgFileName = path.basename(file, path.extname(file));
+                    const componentCode = svgComponentTemplate(svgContent, svgFileName);
+                    mkdirp.sync(outputComponentPath);
+                    fs.writeFileSync(`${outputComponentPath}/${capitalize(svgFileName)}Icon.vue`, componentCode);
+                    console.log(`Generated Vue component for ${svgFileName}`);
+                });
+            }
+        });
+    });
+
 }
 
-fs.readdir(svgFolderPath, (err, files) => {
-  if (err) {
-    console.error(`Error reading SVG folder: ${err}`);
-    process.exit(1);
-  }
-
-  files.forEach((file) => {
-    if (path.extname(file).toLowerCase() === '.svg') {
-      const svgFilePath = path.join(svgFolderPath, file);
-      fs.readFile(svgFilePath, 'utf8', (err, data) => {
-        if (err) {
-          console.error(`Error reading SVG file: ${err}`);
-          return;
-        }
-        const $ = cheerio.load(data);
-        const svgContent = $('svg').html();
-        if (!svgContent) {
-          console.error(`SVG content not found in ${svgFilePath}. Skipping.`);
-          return;
-        }
-        const svgFileName = path.basename(file, path.extname(file));
-        const componentCode = svgComponentTemplate(svgContent, svgFileName);
-        mkdirp.sync(outputComponentPath);
-        fs.writeFileSync(`${outputComponentPath}/${capitalize(svgFileName)}Icon.vue`, componentCode);
-        console.log(`Generated Vue component for ${svgFileName}`);
-      });
-    }
-  });
-});
+module.exports = { convert }
