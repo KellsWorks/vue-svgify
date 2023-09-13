3a0edacfabe48b7ddd0b79c998c6da07f8b92d45
kellworks <kchidothi@outlook.com>
2023-09-13 09:40:40 +0200

    fix: props for height, width, fillColor

--- ComponentTemplate.js
+++ ComponentTemplate.js
@@ -1,22 +1,36 @@
+const replaceFillWithColorProp = (svgContent) => {
+  return svgContent.replace(/fill="[^"]*"/g, ':fill="color"');
+};
+
 module.exports = (svgContent, svgName) => `
 <template>
-  <div>
-    <svg :fillColor="color">
-    ${svgContent}
-    </svg>
-  </div>
+<div>
+  <svg :width="width" :height="height" viewBox="0 0 48 48" :fill="color" xmlns="http://www.w3.org/2000/svg">
+    ${replaceFillWithColorProp(svgContent)}
+  </svg>
+</div>
 </template>
 
 <script lang="ts">
 export default {
-  name: '${svgName.toLowerCase()}_icon',
-  props: {
-    color: {
-        required: false,
-        type: String,
-        default: '#333333'
-    }
+name: '${svgName.toLowerCase()}_icon',
+props: {
+  color: {
+    required: false,
+    type: String,
+    default: '#333333'
+  },
+  height: {
+    required: false,
+    type: Number,
+    default: 48
+  },
+  width: {
+    required: false,
+    type: Number,
+    default: 48
   }
+}
 }
 </script>
 `;
